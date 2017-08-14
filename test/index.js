a60630e4bc4b9bd2d7ea66791c19746c3adc66f2
matt-jarrett <cujarrett@gmail.com>
2017-08-14 15:30:44 -0700

    Adds unit tests for `countOccurrences` and `getLongestElementLength`.

--- test/index.js
+++ test/index.js
@@ -22,18 +22,10 @@ const getInput = () => {
   const inputPath = "./test/input.csv"
   return readFile(inputPath)
 }
+
 const getExpected = () => {
   const expectedPath = "./test/expected.md"
   return readFile(expectedPath)
-}
-
-const test = () => {
-  tape("Test csv-to-md", (assert) => {
-    assert.plan(1)
-    const actual = csvToMd(input)
-    assert.equal(actual, expected, "Converts csv to md as expected")
-    assert.end()
-  })
 }
 
 const setInput = (data) => {
@@ -44,10 +36,44 @@ const setExpected = (data) => {
   expected = data
 }
 
+const endToEndTest = () => {
+  tape("csv-to-md end to end test", (assert) => {
+    assert.plan(1)
+    const actual = csvToMd(input)
+    assert.equal(actual, expected, "Converts csv to md table as expected")
+    assert.end()
+  })
+}
+
+const unitTest = () => {
+  tape("unit tests", (assert) => {
+    assert.plan(4)
+
+    let expected = 2
+    let actual = csvToMd.countOccurrences("JavaScript", "a")
+    assert.equal(actual, expected, "`countOccurrences` positive test")
+
+    expected = 42
+    actual = csvToMd.countOccurrences("JavaScript", "j")
+    assert.notEqual(actual, expected, "`countOccurrences` negative test")
+
+    expected = 10
+    actual = csvToMd.getLongestElementLength(["Java", "JavaScript", "Ruby"])
+    assert.equal(actual, expected, "`getLongestElementLength` positive test")
+
+    expected = 42
+    actual = csvToMd.getLongestElementLength(["Java", "JavaScript", "Ruby"])
+    assert.notEqual(actual, expected, "`getLongestElementLength` negative test")
+
+    assert.end()
+  })
+}
+
 getInput()
   .then((data) => setInput(data))
   .then(() => getExpected())
   .then((data) => setExpected(data))
-  .then(() => test())
+  .then(() => unitTest())
+  .then(() => endToEndTest())
   // eslint-disable-next-line no-console
   .catch((error) => console.log(error))
